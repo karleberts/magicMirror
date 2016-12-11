5be744caeae203554ed738c72738b3a42fe580ec
Karl <karl.eberts@gmail.com>
2016-12-10 19:03:44 -0800

    face detection script in python, native errything

--- services/faceDetection/main.py
+++ services/faceDetection/main.py
@@ -1,36 +1,87 @@
-import os
+from __future__ import print_function
+#import ptvsd
+#ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
+#ptvsd.wait_for_attach()
+"""detect faces and report to the event_bus"""
 import json
 import imp
+# import io
+import os
+import sys
+import time
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import picamera
 from tornado import ioloop
+# from rx.subjects import ReplaySubject, Subject
+from rx import Observable
 
-event_bus = imp.load_source('client', '../eventBus/client.py');
+DIRNAME = os.path.dirname(__file__)
+EVENT_BUS = imp.load_source('client', os.path.join(DIRNAME, '..', 'eventBus', 'client.py'))
+CONFIG_FILE = open(os.path.join(DIRNAME, '..', '..', 'config.json'), 'r')
+CASCADE_PATH = os.path.join(DIRNAME, 'haarcascade_frontalface_default.xml')
+CONFIG = json.load(CONFIG_FILE)
+FACE_CASCADE = cv2.CascadeClassifier(CASCADE_PATH)
+ENDPOINT_ID = 'magicMirror.faceDetect'
 
-from rx.subjects import ReplaySubject, Subject;
-from rx import Observable;
+timer = Observable.interval(500);
 
-config_file = open('../../config.json', 'r')
-config = json.load(config_file);
-port = config['ports']['eventBus']
-endpoint_id = 'magicMirror.faceDetect';
+def get_image_as_array(camera):
+    """grab a frame and convert to a numpy array"""
+    # image = cv2.imread(os.path.join(DIRNAME, 'snapshot.jpg'))
+    raw_capture = PiRGBArray(camera)
+    t = time.time()
+    camera.capture(raw_capture, format='bgr', use_video_port=True)
+    image = raw_capture.array
+    u = time.time()
+    elapsed = u - t
+    print('{} capture'.format(elapsed))
+    return image
+def find_faces_in_image(image):
+    """Find faces in the given opencv numpy array"""
+    t = time.time()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    u = time.time()
+    elapsed = u - t
+    print('{} converting to gray'.format(elapsed))
+    t = time.time()
+    faces = FACE_CASCADE.detectMultiScale(gray)
+    u = time.time()
+    elapsed = u - t
+    print('{} searching faces'.format(elapsed))
+    return len(faces)
+def get_msg_handler(camera):
+    def on_msg(msg):
+        """event_bus message handler"""
+        try:
+            image = get_image_as_array(camera)
+            face_count = find_faces_in_image(image)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception:
+            camera.close()
+            sys.exit()
+        print("found {0} faces".format(face_count))
+        EVENT_BUS.send_message('faceDetect.result', (face_count > 0))
+    return on_msg
 
 
 def start():
-    tests_messages = event_bus.subscribe('test');
-    def on_msg(msg):
-        print 'karl';
-        print msg;
-    tests_messages.subscribe(on_msg);
-    def msg_sender(x):
-        event_bus.send_message('foo', {
-            'test': 'foo'
-        });
-        
-    Observable.interval(5000) \
-        .subscribe(msg_sender);
+    """Callback when event_bus connection succeeds"""
+    print('connected to event bus')
+    tests_messages = EVENT_BUS.subscribe('test')
+    camera = PiCamera(resolution='320x240')
+    camera.start_preview();
+    on_msg = get_msg_handler(camera)
+    # tests_messages.subscribe(on_msg)
+    time.sleep(1);
+    timer.subscribe(on_msg);
 
 def main():
-    event_bus.connect(endpoint_id, start);
-    print 'hi';
+    """kick this party off"""
+    EVENT_BUS.connect(ENDPOINT_ID, start)
+    print('running')
 
 
 if __name__ == '__main__':
